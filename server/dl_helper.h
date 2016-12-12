3c0fed73cf7ba969910e4733e01ecd1436967f60
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2016-12-12 19:09:35 +0100

    server/dl_helper.h: Mac OS X uses ".dylib" instead of ".so" suffix for shared libraries

--- server/dl_helper.h
+++ server/dl_helper.h
@@ -18,41 +18,46 @@
 #endif
 
 
-/* development: prefer dlopen() to lt_dlopen() */
-#define TW_PREFER_DLOPEN
-
-
-#if defined(TW_PREFER_DLOPEN) && defined(TW_HAVE_DLFCN_H) && defined(TW_HAVE_DLOPEN)
-#  undef TW_HAVE_LTDL
-#  undef TW_HAVE_INCLUDED_LTDL
+/* on unix-like systems, prefer dlopen() to lt_dlopen() if both are available */
+#ifdef __unix__
+# define TW_PREFER_DLOPEN
 #endif
 
 
-#if defined(TW_HAVE_LTDL) || defined(TW_HAVE_INCLUDED_LTDL)
-#  include <ltdl.h>
-#  define dlinit         lt_dlinit
+#if defined(TW_HAVE_DLFCN_H) && defined(TW_HAVE_DLOPEN) && (defined(TW_PREFER_DLOPEN) || !defined(TW_HAVE_LTDL) || !defined(TW_HAVE_INCLUDED_LTDL))
+# include <dlfcn.h>
+# undef  dlinit           /* dlopen() requires no initialization */
+# define dlinit_once()    TRUE
+# define dlhandle         void *
+# define dlopen(name)     dlopen((name), RTLD_NOW|RTLD_GLOBAL)
+# define DL_PREFIX        "lib"
+# ifdef __MACOSX__
+#  define DL_EXT            ".dylib"
+# else
+#  define DL_EXT            ".so"
+# endif
+# define DL_SUFFIX        "-" TWIN_VERSION_STR DL_EXT
+
+#elif defined(TW_HAVE_LTDL) || defined(TW_HAVE_INCLUDED_LTDL)
+
+# include <ltdl.h>
+# define dlinit         lt_dlinit
 byte dlinit_once(void);                     /* from dl_helper.c */
-#  define dlerror        lt_dlerror
-#  define dlhandle       lt_dlhandle
-#  define dlopen(name)   lt_dlopenext(name) /* let ltdl decide library name suffix: .la .so .dll or whatever is on the target platform */
-#  define dlclose        lt_dlclose
-#  define dlsym          lt_dlsym
-#  ifdef TW_LT_LIBPREFIX
-#    define DL_PREFIX TW_LT_LIBPREFIX
-#  else
-#    define DL_PREFIX ""
-#  endif
-#  define DL_SUFFIX   ""
-#elif defined(TW_HAVE_DLFCN_H) && defined(TW_HAVE_DLOPEN)
-#  include <dlfcn.h>
-#  undef  dlinit           /* dlopen() requires no initialization */
-#  define dlinit_once()    TRUE
-#  define dlhandle         void *
-#  define dlopen(name)     dlopen((name), RTLD_NOW|RTLD_GLOBAL)
-#  define DL_PREFIX        "lib"
-#  define DL_SUFFIX        "-" TWIN_VERSION_STR ".so"
+# define dlerror        lt_dlerror
+# define dlhandle       lt_dlhandle
+# define dlopen(name)   lt_dlopenext(name) /* let ltdl decide library name suffix: .la .so .dll or whatever is on the target platform */
+# define dlclose        lt_dlclose
+# define dlsym          lt_dlsym
+# ifdef TW_LT_LIBPREFIX
+#  define DL_PREFIX TW_LT_LIBPREFIX
+# else
+#  define DL_PREFIX ""
+# endif
+# define DL_EXT     ""
+# define DL_SUFFIX  ""
+
 #else
-#  error nor dlopen() nor lt_dlopen() module loading API available!  
+# error nor dlopen() nor lt_dlopen() module loading API available!  
 #endif
 
 #endif /* _TWIN_DL_HELPER_H */
