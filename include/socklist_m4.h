c61c653930401810d62f947175888f2888ee1998
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2025-06-05 19:51:52 +0200

    format include/socklist_m4.h

--- include/socklist_m4.h
+++ include/socklist_m4.h
@@ -1,7 +1,4 @@
 
-
-
-
 /* This file was automatically generated from m4/socklist.m4, do not edit!
  *  Copyright (C) 2002 by Massimiliano Ghilardi
  *
@@ -11,67 +8,26 @@
  *  version 2 of the License, or (at your option) any later version.
  *
  */
-
 /* clang-format off */
 
-
-
-
-
-
-
-
-#define OK_MAGIC        ((uldat)0x3E4B4F3Cul)
-#define FAIL_MAGIC        ((uldat)0xFFFFFFFFul)
-
-#define FIND_MAGIC                ((uldat)0x646E6946ul) /* i.e. "Find" */
-#define MSG_MAGIC                ((uldat)0x2167734dul) /* i.e. "Msg!" */
-
-
-
-
-
-
-
-
-
-
-
+#define OK_MAGIC      ((uldat)0x3E4B4F3Cul)
+#define FAIL_MAGIC    ((uldat)0xFFFFFFFFul)
+#define FIND_MAGIC    ((uldat)0x646E6946ul) /* i.e. "Find" */
+#define MSG_MAGIC     ((uldat)0x2167734dul) /* i.e. "Msg!" */
 
 EL(FindFunction)
-
-
 EL(SyncSocket)
-
-
 EL(ServerSizeof)
-
-
 EL(CanCompress)
-
 EL(DoCompress)
-
-
 EL(NeedResizeDisplay)
-
-
 EL(AttachHW)
-
 EL(DetachHW)
-
-
 EL(SetFontTranslation)
-
 EL(SetTRuneTranslation)
-
-
 EL(DeleteObj)
-
 EL(ChangeFieldObj)
-
-
 EL(CreateWidget)
-
 EL(RecursiveDeleteWidget)
 EL(MapWidget)
 EL(UnMapWidget)
@@ -79,68 +35,48 @@ EL(SetXYWidget)
 EL(ResizeWidget)
 EL(ScrollWidget)
 EL(DrawWidget)
-
 EL(SetVisibleWidget)
-
 EL(FocusSubWidget)
 EL(FindWidgetAtWidget)
-
 EL(RaiseWidget)
 EL(LowerWidget)
 EL(RestackChildrenWidget)
 EL(CirculateChildrenWidget)
-
-
 EL(CreateGadget)
-
 EL(CreateButtonGadget)
-
 EL(WriteTextsGadget)
 EL(WriteTRunesGadget)
-
 EL(CreateWindow)
 EL(Create4MenuWindow)
-
 EL(WriteCharsetWindow)
 EL(WriteUtf8Window)
 EL(WriteTRuneWindow)
 EL(WriteTCellWindow)
-
 EL(GotoXYWindow)
 EL(SetTitleWindow)
 EL(SetColTextWindow)
 EL(SetColorsWindow)
 EL(ConfigureWindow)
 EL(FindRowByCodeWindow)
-
 EL(CreateGroup)
 EL(InsertGadgetGroup)
 EL(RemoveGadgetGroup)
-
 EL(GetSelectedGadgetGroup)
 EL(SetSelectedGadgetGroup)
-
 EL(RaiseRow)
 EL(LowerRow)
 EL(RestackChildrenRow)
 EL(CirculateChildrenRow)
-
 EL(Create4MenuAny)
-
 EL(Create4MenuCommonMenuItem)
-
 EL(CreateMenu)
 EL(SetInfoMenu)
-
 EL(CreateMsgPort)
 EL(FindMsgPort)
-
 EL(BgImageScreen)
-
 EL(PrevObj)
 EL(NextObj)
 EL(ParentObj)
-
 EL(FirstScreen)
 EL(FirstWidget)
 EL(FirstMsgPort)
@@ -150,18 +86,13 @@ EL(FirstGroup)
 EL(FirstMutex)
 EL(FirstMenuItem)
 EL(FirstGadget)
-
 EL(GetDisplayWidth)
 EL(GetDisplayHeight)
 EL(GetAll)
-
 EL(SendToMsgPort)
 EL(BlindSendToMsgPort)
-
 EL(GetOwnerSelection)
 EL(SetOwnerSelection)
 EL(RequestSelection)
 EL(NotifySelection)
-
 EL(SetServerUid)
-
