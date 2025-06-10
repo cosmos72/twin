032849bc23bf025234b5579df5d37e8a1be50725
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2025-06-10 18:39:19 +0200

    remove redundant macro XDRAW_ANY()

--- server/hw/hw_X11.cpp
+++ server/hw/hw_X11.cpp
@@ -52,8 +52,6 @@
     myXDrawImageString(xdisplay, xwindow, xgc, xbegin, ybegin + xupfont, buf, buflen);             \
   } while (0)
 
-#define XDRAW_ANY(buf, buflen, col, _) XDRAW(col, buf, buflen)
-
 #include "hw_x/util.h"
 #include "hw_x/common.h"
 
